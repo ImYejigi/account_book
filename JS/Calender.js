9546a399078439fdb4502f727e30cc5851f0c160
Yejigi <jyj000207@naver.com>
2022-10-24 11:51:21 +0900

    2201024 Update
    코드 병합

---
+++ JS/Calender.js
@@ -0,0 +1,31 @@
+const date = new Date();
+
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+
+document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+
+const prevLast = new Date(viewYear, viewMonth, 0);
+const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+const PLDate = prevLast.getDate();
+const PLDay = prevLast.getDay();
+
+const TLDate = thisLast.getDate();
+const TLDay = thisLast.getDay();
+
+const prevDates = [];
+const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+const nextDates = [];
+
+if (PLDay != 6) {
+    for(let i = 0 ; i < PLDay + 1; i++) {
+        prevDates.unshift(PLDate - i);
+    }
+}
+
+for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+}
+
+const dayes = prevDayes.concat(thisDates, nextDates);
