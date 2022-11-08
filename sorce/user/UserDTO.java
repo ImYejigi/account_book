6af56940bf47367b812ab4494e538d70f7f8142d
qwerzv <112801938+qwerzv@users.noreply.github.com>
2022-11-08 10:13:20 +0900

    Add files via upload

---
+++ sorce/user/UserDTO.java
@@ -0,0 +1,37 @@
+package user;
+
+public class UserDTO {
+	
+		private String userID;
+		private String userPassword;
+		private String userNmae;
+		private String userEmail;
+		public String getUserID() {
+			return userID;
+		}
+		public void setUserID(String userID) {
+			this.userID = userID;
+		}
+		public String getUserPassword() {
+			return userPassword;
+		}
+		public void setUserPassword(String userPassword) {
+			this.userPassword = userPassword;
+		}
+		public String getUserNmae() {
+			return userNmae;
+		}
+		public void setUserNmae(String userNmae) {
+			this.userNmae = userNmae;
+		}
+		public String getUserEmail() {
+			return userEmail;
+		}
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
+
+
+}
+
+		
